26a0f7a1169d408ebf702da600251355475bfbe4
João Paulo <joaoantunees@gmail.com>
2023-03-10 17:32:22 -0300

    Feat: Incluido rotas

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+
 const app = express()
 
 app.use(cors())
@@ -10,6 +11,11 @@ app.use(express.json())
 const conn = require('./db/conn')
 
 conn()
+
+// Routes
+const routes = require('./routes/router')
+
+app.use('/api', routes)
 
 app.listen(3000, function(){
   console.log("Servidor online!")
